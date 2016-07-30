79e9ab23c3a5aa17d21550e02c42ac2bd2738420
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-07-30 12:11:51 -0600

    initial commit, scaffold and some React class set up

---
+++ src/components/ProgressSoundPlayer.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+import { SoundCloudContainer } from 'react-soundplayer/addons';
+import Track from './Track';
+
+export default class ProgressSoundPlayer extends Component {
+  render() {
+    const { resolveUrl, clientId } = this.props;
+    return (
+      <SoundCloudContainer resolveUrl={ resolveUrl } clientId={ clientId }>
+        <Track>
+      </SoundCloudContainer>
+    );
+  }
+}
